80e4afbb5263e9065bdb7c882ec1b39d91cb1eee
Kiranadil <kiranadil10@gmail.com>
2021-06-03 02:06:53 +0500

    print out the variables

---
+++ JS coding TASLS 2/Operators/arithmeticOperator/app.js
@@ -0,0 +1,10 @@
+ //Create two variables
+
+ var var1 = 9;
+ var var2 = 5;
+ var sum = Number(var1) +  Number(var2);
+ var multiply = Number(var1) * Number(var2);
+ var divide = Number(var1)/Number(var2);
+  alert(" The sum of the numbers is: " +sum);//alert their sum
+  alert("The multiplication of the numbers is: " +multiply);//alert their multiplication
+  alert("The division of the numbers is: " +divide);//alert their division
---
+++ JS coding TASLS 2/Variables/variableName/app.js
@@ -0,0 +1,37 @@
+/*Declare a variable using let inside a function.Add that 
+same variable in another function and see what happens.*/
+
+function variableName1(){
+    let a = 5 ;
+    console.log(a);
+}
+
+function variableName2(){
+      a;
+    console.log(a);
+}
+
+//It is showing "can't find the variable".
+//This is because "let" scopes the variable to the local only in which it is declared.
+
+/*Now declare a variable using var in a block and try reassigning the value to that
+ variable in another block and see if it throws an error or not.*/
+ function firstFunction(){
+     var b = 9;
+ }
+ console.log(b);
+
+ //Yes it is throwing an error--->because it isn't declared as global
+
+ //Create a global variable 
+ var x = 10;
+// Here x is 10
+ function functionOne() {
+  let x = 2;
+  // Here x is 2
+}
+function functionTwo(){
+    let y =x+2;
+    console.log(y);
+}
+  console.log(x);
