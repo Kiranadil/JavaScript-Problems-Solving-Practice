9bea3bf0b8c317e6040fc1a13e88282c8d0b1437
Kiranadil <kiranadil10@gmail.com>
2021-06-11 01:37:04 +0500

    checked empty fields

--- JS coding TASKS 9/FORMS/Forms/app.js
+++ JS coding TASKS 9/FORMS/Forms/app.js
@@ -1,7 +1,19 @@
 
-document.getElementById('btn').addEventListener("click", function() {
-    if(document.getElementsByClassName("email").value == 0)
-{
-    alert("empty");
+document.getElementById('btn').onclick = clickMe;
+function clickMe() {
+    let element1 = document.getElementById('inp1');
+    let element2 = document.getElementById('inp1');
+
+    if (element1.value == null && element2.value == null) {
+        alert("Fill the empty spaces");
+    }
+    else if (element1.value == null || element2.value == null){
+        alert('Some blanks are not filled');
+    }
+    else {
+        alert('Logged In Successfully')
+    }
+    if (element2 < 8){
+        alert('Length is too short')
+    }
 }
-});
