5ddae327154c4415164fd601441d304602e81c0c
Kiranadil <kiranadil10@gmail.com>
2021-06-10 23:42:20 +0500

    added a click event

---
+++ JS coding TASKS 9/DOM/JSDOM/app.js
@@ -0,0 +1,15 @@
+//get the reference of first div using DOM methods
+var x = document.getElementsByClassName("div-class")[0]; 
+console.log(x);
+
+//Change attribute class of this div to ‘changed-class’
+var y = x.className = "changed-class";
+console.log(y);
+
+//change the style attribute of that div to give red color to the text 
+x.style.color = "red";
+
+//give solid blue border to it.
+x.style.border = " 2px solid blue";
+
+
---
+++ JS coding TASKS 9/FORMS/Forms/app.js
@@ -0,0 +1,7 @@
+
+document.getElementById('btn').addEventListener("click", function() {
+    if(document.getElementsByClassName("email").value == 0)
+{
+    alert("empty");
+}
+});
